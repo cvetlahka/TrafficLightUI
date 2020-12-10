973455aec3731db970eb92a3bcd8306a43b5d20f
Safonova Svetlana <Safonova_s@icloud.com>
2020-12-10 13:51:26 +0300

    Initial Commit

---
+++ TrafficLightUI/TrafficLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightUIApp.swift
+//  TrafficLightUI
+//
+//  Created by Светлана Сафонова on 10.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
